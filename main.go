4c6d961bbe0996d5619ff206c94f00e145135052
Dmitriy Bulygin <demetrius.storm@gmail.com>
2024-01-09 22:22:18 +0300

    added db model struct and todo stuct to frontend display

--- main.go
+++ main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"time"
+
+	"github.com/thedevsaddam/renderer"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var (
+	rnd    *renderer.Render
+	client *mongo.Client
+	db     *mongo.Database
+)
+
+const (
+	dbName         string = "golang-todo"
+	collectionName string = "todo"
+)
+
+// that the Frontend will display
+type (
+	TodoModel struct {
+		ID        primitive.ObjectID `bson:"id,omitempty"`
+		Title     string             `bson:"title"`
+		Completed bool               `bson:"completed"`
+		CreatedAt time.Time          `bson:"created_at"`
+	}
+
+	Todo struct {
+		ID        string    `json:"id"`
+		Title     string    `json:"title"`
+		Completed bool      `json:"completed"`
+		CreatedAt time.Time `json:"created_at"`
+	}
+)
