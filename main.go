c7d8edf0b98978ebe7c46811929c5726eac4b316
Dmitriy Bulygin <demetrius.storm@gmail.com>
2024-01-09 22:32:03 +0300

    created an init function and connect to a database

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"time"
 
 	"github.com/thedevsaddam/renderer"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 var (
@@ -35,3 +39,21 @@ type (
 		CreatedAt time.Time `json:"created_at"`
 	}
 )
+
+func init() {
+	fmt.Println("init func running")
+
+	rnd = renderer.New()
+	var err error
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	fmt.Println(err)
+
+	err = client.Ping(ctx, readpref.Primary())
+	fmt.Println(err)
+
+	db = client.Database(dbName)
+}
